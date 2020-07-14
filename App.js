2027a479b71169009041fab7d79c68841c8054e6
Equihua <luis.herrerae@outlook.com>
2020-07-13 21:11:16 -0500

    code deleted

--- App.js
+++ App.js
@@ -1,39 +1,18 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import {
-  StyleSheet,
-  Text,
-  Image,
-  TouchableWithoutFeedback,
-  SafeAreaView,
-} from "react-native";
+import React from 'react'
+import { SafeAreaView, StyleSheet } from 'react-native'
+import ItemDisplay from './pages/item-display/ItemDisplay'
 
 export default function App() {
-  const handlePressText = () => console.log("Text pressed");
-  const handlePressImage = () => console.log("Nice image");
-
   return (
     <SafeAreaView style={styles.container}>
-      <Text onPress={handlePressText}>I love React damn ❤!</Text>
-      <TouchableWithoutFeedback onPress={handlePressImage}>
-        <Image
-          source={{
-            uri: "https://picsum.photos/200/300",
-            width: 200,
-            height: 300,
-          }}
-        />
-      </TouchableWithoutFeedback>
-      <StatusBar style="auto" />
+      <ItemDisplay />
     </SafeAreaView>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
   },
-});
+})
